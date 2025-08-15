11b4f8763336c779810748b361a225a22b035a02
nikolamilosa <nikolamilosa20@gmail.com>
2025-08-16 01:54:43 +0200

    fixing imports

--- src/target_topology_frontend/src/routes/MainRoutes.jsx
+++ src/target_topology_frontend/src/routes/MainRoutes.jsx
@@ -3,19 +3,8 @@ import { lazy } from 'react';
 import AdminLayout from '../layouts/AdminLayout';
 import GuestLayout from '../layouts/GuestLayout';
 
-const DashboardSales = lazy(() => import('../views/dashboard/DashSales/index'));
+const DashboardHome = lazy(() => import('../views/home/DashHome/index'));
 
-const Typography = lazy(() => import('../views/ui-elements/basic/BasicTypography'));
-const Color = lazy(() => import('../views/ui-elements/basic/BasicColor'));
-
-const FeatherIcon = lazy(() => import('../views/ui-elements/icons/Feather'));
-const FontAwesome = lazy(() => import('../views/ui-elements/icons/FontAwesome'));
-const MaterialIcon = lazy(() => import('../views/ui-elements/icons/Material'));
-
-const Login = lazy(() => import('../views/auth/login'));
-const Register = lazy(() => import('../views/auth/register'));
-
-const Sample = lazy(() => import('../views/sample'));
 
 const MainRoutes = {
   path: '/',
@@ -25,33 +14,8 @@ const MainRoutes = {
       element: <AdminLayout />,
       children: [
         {
-          path: '/dashboard/sales',
-          element: <DashboardSales />
-        },
-        {
-          path: '/typography',
-          element: <Typography />
-        },
-        {
-          path: '/color',
-          element: <Color />
-        },
-        {
-          path: '/icons/Feather',
-          element: <FeatherIcon />
-        },
-        {
-          path: '/icons/font-awesome-5',
-          element: <FontAwesome />
-        },
-        {
-          path: '/icons/material',
-          element: <MaterialIcon />
-        },
-
-        {
-          path: '/sample-page',
-          element: <Sample />
+          path: '/home',
+          element: <DashboardHome />
         },
         {
           path: '*',
@@ -62,16 +26,7 @@ const MainRoutes = {
     {
       path: '/',
       element: <GuestLayout />,
-      children: [
-        {
-          path: '/login',
-          element: <Login />
-        },
-        {
-          path: '/register',
-          element: <Register />
-        }
-      ]
+      children: []
     }
   ]
 };

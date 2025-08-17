92e1e341b1ebfba64f5dfb3ea7be2fe324ed248f
nikolamilosa <nikolamilosa20@gmail.com>
2025-08-17 21:38:47 +0200

    using a different icon

--- src/target_topology_frontend/src/views/subnetDetail/SubnetDetail/index.jsx
+++ src/target_topology_frontend/src/views/subnetDetail/SubnetDetail/index.jsx
@@ -169,7 +169,7 @@ export default function SubnetDetail() {
               <Row>
                 {topologyReport.map((x, k) => (
                   <Col sm={12} md={6} key={k}>
-                    <ProductCard params={{ variant: x.violations.length == 0 ? 'success' : 'danger', title: x.limit_name, primaryText: x.limit_value, icon: 'network_check', secondaryText: x.violations.length == 0 ? 'Constraint is enforced.' : (
+                    <ProductCard params={{ variant: x.violations.length == 0 ? 'success' : 'danger', title: x.limit_name, primaryText: x.limit_value, icon: 'layers', secondaryText: x.violations.length == 0 ? 'Constraint is enforced.' : (
                       <>
                         <span>Violations found: </span>
                         <ul>
